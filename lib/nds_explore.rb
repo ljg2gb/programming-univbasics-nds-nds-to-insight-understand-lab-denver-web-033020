6747775e6423dbda4f993f1c0134c5b78b62b4d4
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-12 19:35:46 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,12 +1,54 @@
 require 'pp'
+require 'pry'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  pp directors_database
+  #binding.pry
+  pp nds
 end
 
 def print_first_directors_movie_titles
+   
+  # first_array
+  row_index = 0
+  movies = directors_database[0][:movies]
+    while row_index < movies.length do
+      #binding.pry
+      puts movies[row_index][:title]
+      row_index += 1
+    end
+  # while row_index < directors_database.length do
+    
+  #   #first Hash
+  #   column_index = 0
+  #   column_len = directors_database[row_index].length
+  #   while column_index < column_len do
+      
+  #     #inner Array
+  #     inner_len = directors_database[row_index][:movies].length
+  #     inner_index = 0
+  #     while inner_index < inner_len do
+        
+  #       #inner Hash
+  #       inner_inner_len = directors_database[row_index][:movies][inner_index].length
+  #       inner_inner_index = 0
+  #       while inner_inner_index < inner_inner_len do
+  #         title_name = directors_database[row_index][:movies][inner_index][:title]
+  #         puts "#{title_name}\n"
+  #         inner_inner_index += 1
+  #       end
+        
+  #       inner_index += 1
+  #     end
+ 
+  #     column_index += 1
+  #   end
+ 
+  #   row_index += 1
+  # end
+  
 end
+#pretty_print_nds(directors_database)
