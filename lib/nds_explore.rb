5e8b9d23f95360b9c889ea1ea1ed875d55b2c420
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-12 19:20:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,39 +14,39 @@ def print_first_directors_movie_titles
    
   # first_array
   row_index = 0
-  movies = directors_database[0][:movies]
-    while row_index < movies do
-       binding.pry
-    end
-  # while row_index < directors_database.length do
-    
-  #   #first Hash
-  #   column_index = 0
-  #   column_len = directors_database[row_index].length
-  #   while column_index < column_len do
-      
-  #     #inner Array
-  #     inner_len = directors_database[row_index][:movies].length
-  #     inner_index = 0
-  #     while inner_index < inner_len do
-        
-  #       #inner Hash
-  #       inner_inner_len = directors_database[row_index][:movies][inner_index]
-  #       inner_inner_index = 0
-  #       while inner_inner_index < inner_inner_len do
-  #         title_name = directors_database[row_index][:movies][inner_index][:title]
-  #         puts "#{title_name}\n"
-  #         inner_inner_index += 1
-  #       end
-        
-  #       inner_index += 1
-  #     end
- 
-  #     column_index += 1
+  # movies = directors_database[0][:movies]
+  #   while row_index < movies.length do
+  #     binding.pry
   #   end
- #
-   # row_index += 1
-  #end
+  while row_index < directors_database.length do
+    
+    #first Hash
+    column_index = 0
+    column_len = directors_database[row_index].length
+    while column_index < column_len do
+      
+      #inner Array
+      inner_len = directors_database[row_index][:movies].length
+      inner_index = 0
+      while inner_index < inner_len do
+        
+        #inner Hash
+        inner_inner_len = directors_database[row_index][:movies][inner_index].length
+        inner_inner_index = 0
+        while inner_inner_index < inner_inner_len do
+          title_name = directors_database[row_index][:movies][inner_index][:title]
+          puts "#{title_name}\n"
+          inner_inner_index += 1
+        end
+        
+        inner_index += 1
+      end
+ 
+      column_index += 1
+    end
+ 
+    row_index += 1
+  end
   
 end
 #pretty_print_nds(directors_database)
